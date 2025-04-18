53f6a8299813a6175d91bf46e7c872a7ecb07d23
Felipe Ferreira <felipeabferreira22@gmail.com>
2025-04-17 21:20:27 -0300

    Reestruturação do código em diferentes arquivos .py

--- core.py
+++ core.py
@@ -1,11 +1,23 @@
 from PIL import Image, ImageDraw, ImageFont, ImageTk
 import tkinter as tk
-from tkinter import StringVar, messagebox, IntVar, filedialog, ttk
+from tkinter import messagebox, filedialog, ttk
 import os
 
 imagem_base = None
 posicao_inicial_arraste = None
 imagem_tk = None
+
+texto = None
+numero_inicial = None
+numero_final = None
+eixoX = None
+eixoY = None
+cor_texto = None
+tamanho_fonte = None
+fontes_disponiveis = None
+fonte_escolhida = None
+preview = None
+janela = None
 
 def iniciar_arraste(event):
     global posicao_inicial_arraste
@@ -42,7 +54,7 @@ def alterar_tamanho_fonte(event):
 
         current_size = tamanho_fonte.get()
         new_size = current_size + (5 * change)
-        new_size = max(10, min(200, new_size))
+        new_size = max(10, min(500, new_size))
 
         tamanho_fonte.set(new_size)
 
@@ -174,6 +186,9 @@ def gerar_imagem():
             desenho_temp = ImageDraw.Draw(imagem_temp)
             desenho_temp.text(posicao, texto_final, font = fonte_atual, fill = cor_texto_atual)
 
+            if imagem_temp.mode == 'RGBA':
+                imagem_temp = imagem_temp.convert('RGB')
+
             caminho = os.path.join(gerar_imagem.pasta_destino, f"imagem_{numero_formatado}.jpg")
             imagem_temp.save(caminho)
         
@@ -186,65 +201,3 @@ def gerar_imagem():
         if 'e' not in locals() or isinstance(e, (PermissionError, Exception)):
             messagebox.showinfo('Sucesso', f"{total_de_imagens} imagens geradas com sucesso!")
         janela.destroy()
-
-janela = tk.Tk()
-janela.title("MultiMage")
-janela.geometry("1025x350")
-
-texto = StringVar()
-numero_inicial = IntVar(value = 1)
-numero_final = IntVar(value = 10)
-eixoX = IntVar(value = 100)
-eixoY = IntVar(value = 100)
-cor_texto = StringVar(value = 'black')
-tamanho_fonte = IntVar(value = 100)
-fontes_disponiveis = ["arial.ttf", "cour.ttf", "time.ttf"]
-fonte_escolhida = StringVar(value = fontes_disponiveis[0])
-
-config_div = tk.LabelFrame(janela, text = 'Configurações', padx = 10, pady = 10)
-config_div.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = 'n')
-
-preview_div = tk.LabelFrame(janela, text = 'Preview', padx = 10, pady = 10)
-preview_div.grid(row = 0, column = 1, padx = 20, pady = 10)
-
-btn_div = tk.Frame(janela)
-btn_div.grid(row = 1, column= 0, columnspan=2, pady = 20)
-
-tk.Label(config_div,text = "Texto base:").grid(row = 0, column=0,sticky='w')
-tk.Entry(config_div,textvariable = texto,width = 50,bd = 2).grid(row = 0, column=1)
-
-tk.Label(config_div, text = "Escolha a cor do texto:").grid(row=1, column=0, sticky="w")
-tk.Entry(config_div, textvariable = cor_texto,).grid(row=1, column=1)
-
-tk.Label(config_div, text = "Escolha o tamanho da fonte:").grid(row=2, column=0, sticky="w")
-tk.Entry(config_div, textvariable = tamanho_fonte,).grid(row=2, column=1)
-
-tk.Label(config_div, text = "Escolha a fonte:").grid(row=3, column=0, sticky="w")
-ttk.Combobox(config_div, textvariable = fonte_escolhida, values = fontes_disponiveis, state = 'readonly').grid(row=3, column=1)
-
-tk.Label(config_div, text = "Número inicial:").grid(row=4, column=0, sticky="w")
-tk.Entry(config_div, textvariable = numero_inicial).grid(row=4, column=1)
-
-tk.Label(config_div, text = "Número Final:").grid(row=5, column=0, sticky="w")
-tk.Entry(config_div, textvariable = numero_final).grid(row=5, column=1)
-
-tk.Label(config_div, text = "Posição horizontal (px):").grid(row=6, column=0, sticky="w")
-tk.Entry(config_div, textvariable = eixoX).grid(row=6, column=1)
-
-tk.Label(config_div, text = "Posição vertical (px):").grid(row=7, column=0, sticky="w")
-tk.Entry(config_div, textvariable = eixoY).grid(row=7, column=1)
-
-preview = tk.Label(preview_div)
-preview.pack()
-
-tk.Button(btn_div,text = 'Selecionar Imagem', command = selecionar_imagem).grid(row=0, column=0, padx=10)
-tk.Button(btn_div,text = 'Visualizar Preview', command = mostrar_preview).grid(row=0, column=1, padx=10)
-tk.Button(btn_div,text = 'Selecionar Pasta',command = selecionar_pasta).grid(row=0, column=2, padx=10)
-tk.Button(btn_div,text = 'Gerar Imagem',command = gerar_imagem).grid(row=0, column=3, padx=10)
-
-preview.bind("<Button-1>", iniciar_arraste)
-preview.bind("<B1-Motion>",mover_texto)
-preview.bind("<ButtonRelease-1>",finalizar_arraste)
-preview.bind("<Enter>", configurar_scroll)
-
-janela.mainloop()
---
+++ main.py
@@ -0,0 +1,4 @@
+from ui import criar_janela
+
+if __name__ == "__main__":
+    criar_janela()
